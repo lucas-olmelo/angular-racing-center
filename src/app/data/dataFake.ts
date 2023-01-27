2118429bdb64f5a7e8d7379dc72b247ca869e5c7
lucas-olmelo <lucaas1904@gmail.com>
2023-01-27 00:16:55 -0300

    adição de nova página

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: 1,
+    imageCover: 'https://assets3.thrillist.com/v1/image/2339422/1200x600/scale;',
+    title: 'A Estrela da Morte destroi Alderaan',
+    description: 'O império demonstra a força de sua mais nova arma ao destruir o planeta natal da líder rebelde Princesa Leia'
+  },
+  {
+    id: 2,
+    imageCover: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/emperor-palpatine-1555323736.jpg?crop=0.853xw:1.00xh;0,0&resize=1200:*',
+    title: 'O Imperador Palpatine ficou satisfeito com o desempenho da nova arma',
+    description: 'ola'
+  },
+  {
+    id: 3,
+    imageCover: 'https://lumiere-a.akamaihd.net/v1/images/607598d0230e6a00018e21b2-image_354b1b56.jpeg?region=0%2C48%2C1536%2C768',
+    title: 'Darth Vader e o governador Tarkin comandam a ação da nova estação',
+    description: 'ola'
+  },
+  {
+    id: 4,
+    imageCover: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Princess-Leia.Star-Wars-Series.webp',
+    title: 'Apesar da pressão imperial, Leia Organa não entregou a localização da base rebelde"',
+    description: 'ola'
+  }
+]
