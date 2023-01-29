cd7cfba76c5159181b92bbac7e65f04c2566501a
lucas-olmelo <lucaas1904@gmail.com>
2023-01-29 17:51:15 -0300

    novas notícias

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import noticias_blog from "../../../assets/data/noticias_blog.json";
 
 @Component({
   selector: 'app-big-card',
@@ -8,17 +9,34 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BigCardComponent implements OnInit {
 
   @Input()
+  id: string = '';
+  title: string = '';
   imageCover: string = '';
-  @Input()
-  cardTitle: string = '';
-  @Input()
-  cardDescription: string = '';
-  @Input()
-  Id: string = '0';
+  category: string = '';
+  subtitle: string = '';
+
+  // @Input()
+  // imageCover: string = '';
+  // @Input()
+  // cardTitle: string = '';
+  // @Input()
+  // cardDescription: string = '';
+  // @Input()
+  // Id: string = '0';
 
   constructor() { }
 
   ngOnInit(): void {
+    if (noticias_blog) {
+      const result = noticias_blog.news.filter(article => article.id.toString() == this.id)[0]
+
+      this.title = result.title;
+      this.subtitle = result.subtitle;
+      this.category = result.category;
+      this.imageCover = result.imageCover;
+    } else {
+      console.log('erro');
+    }
   }
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import noticias_blog from "../../../assets/data/noticias_blog.json";
+
 
 @Component({
   selector: 'app-small-card',
@@ -8,15 +10,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SmallCardComponent implements OnInit {
 
   @Input()
+  id: string = '';
+  title: string = '';
   imageCover: string = '';
-  @Input()
-  cardTitle: string = '';
-  @Input()
-  Id: string = '';
+  category: string = '';
+  subtitle: string = '';
+
+  // @Input()
+  // imageCover: string = '';
+  // @Input()
+  // cardTitle: string = '';
+  // @Input()
+  // Id: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    if (noticias_blog) {
+      const result = noticias_blog.news.filter(article => article.id.toString() == this.id)[0]
+
+      this.title = result.title;
+      this.subtitle = result.subtitle;
+      this.category = result.category;
+      this.imageCover = result.imageCover;
+    } else {
+      console.log('erro');
+    }
   }
 
 }
